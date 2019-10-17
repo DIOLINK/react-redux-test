3312a03f3131df103b5bf05e9c53163ab0630c49
Roger <54718297+DIOLINK@users.noreply.github.com>
2019-10-17 15:30:36 -0300

    Redux files creation

---
+++ src/components/redux/reducers/currentitems.js
@@ -0,0 +1,10 @@
+const defaulState = 0;
+
+function reducer(state = defaulState, { type, payload }) {
+    switch (type){
+        default:
+            return state;
+    }
+}
+
+export default reducer;
---
+++ src/components/redux/reducers/suggestions.js
@@ -0,0 +1,10 @@
+const defaulState = [];
+
+function reducer(state = defaulState, { type, payload }) {
+    switch (type){
+        default:
+            return state;
+    }
+}
+
+export default reducer;
